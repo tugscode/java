0584673b19918f81991a1897123a1e463d8ff435
tugs-erdene.o <tugs-erdene.o@unitel.mn>
2022-08-19 18:19:49 +0800

    [ADD] adding l2

---
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -0,0 +1,34 @@
+package academy.learnprogramming;
+
+public class Main {
+    public static void main(String[] args) {
+        int myValue = 10000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted Max Value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Min Value = " +(myMinIntValue -1));
+        int myMaxIntTest = 2147483647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        int myTotal = (myMinIntValue / 2);
+
+        int myNewByteValue = (myMinByteValue / 2);
+    }
+}
--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void  main(String[] args){
-        System.out.println("myFirstNumber");
+        System.out.println("Hello Tugsuu");
 
         int myFirstNumber = (5 + 10) + (2 * 10);
         int mySecondNumber= 12;
