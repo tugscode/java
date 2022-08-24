0c2f3baf70da72a8a3c4a1ba31dbf033471081f6
tugs-erdene.o <tugs-erdene.o@unitel.mn>
2022-08-24 17:54:11 +0800

    [ADD] adding if switch

--- FloatingAndDouble/src/Main.java
+++ FloatingAndDouble/src/Main.java
@@ -1,6 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner ymOruul = new Scanner(System.in);
+
+
+        //if hicheel
+//        int a, b, c, max = 0;
+//
+//
+//        a = ymOruul.nextInt();
+//
+//        b = ymOruul.nextInt();
+//
+//        c = ymOruul.nextInt();
+//
+//        if(a > b && a > c){
+//            max = a;
+//        } else if (b > c) {
+//            max = b;
+//        }else {
+//            max = c;
+//        }
+//        System.out.println(max);
+
+// for davtalt
+//        for(int i = 0; i <= 100; i = i + 2){
+//            System.out.println(i);
+//        }
+
+        //while
+//        int i = 0;
+//        while (i <= 5){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //do while
+
+        int u = 0;
+        do {
+            System.out.println(u);
+            u ++;
+        }while (u < 5);
+
     }
 }
